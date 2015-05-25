3db81f41301463f8a3928bb544e5d5a23073a09e
Arpit Gupta <glex.qsd@gmail.com>
2015-05-25 11:55:59 -0400

    Initial Commit for Second Week's Programming Assignment

---
+++ Programming-Assignments/Programming-Assignment-2/CustomTopo.py
@@ -0,0 +1,25 @@
+'''
+Coursera:
+- Software Defined Networking (SDN) course
+-- Module 3 Programming Assignment
+
+Professor: Nick Feamster
+Teaching Assistant: Muhammad Shahbaz
+'''
+
+from mininet.topo import Topo
+
+class CustomTopo(Topo):
+    "Simple Data Center Topology"
+
+    "linkopts - (1:core, 2:aggregation, 3: edge) parameters"
+    "fanout - number of child switch per parent switch"
+    def __init__(self, linkopts1, linkopts2, linkopts3, fanout=2, **opts):
+        # Initialize topology and default options
+        Topo.__init__(self, **opts)
+        
+        # Add your logic here ...
+
+        
+                    
+topos = { 'custom': ( lambda: CustomTopo() ) }
