6408eac07aed3687823e977efbaab45cdb79319c
Arpit Gupta <glex.qsd@gmail.com>
2015-05-25 15:05:58 -0400

    ..

---
+++ assignments/assignment-2/submit.py
@@ -0,0 +1,201 @@
+'''
+Coursera:
+- Software Defined Networking (SDN) course
+-- Module 3 Programming Assignment
+
+Professor: Nick Feamster
+Teaching Assistant: Muhammad Shahbaz
+'''
+
+### The only things you'll have to edit (unless you're porting this script over to a different language) 
+### are at the bottom of this file.
+
+import urllib
+import urllib2
+import hashlib
+import random
+import email
+import email.message
+import email.encoders
+import StringIO
+import sys
+
+""""""""""""""""""""
+""""""""""""""""""""
+
+class NullDevice:
+  def write(self, s):
+    pass
+
+def submit():   
+  print '==\n== [sandbox] Submitting Solutions \n=='
+  
+  (login, password) = loginPrompt()
+  if not login:
+    print '!! Submission Cancelled'
+    return
+  
+  print '\n== Connecting to Coursera ... '
+
+  # Part Identifier
+  (partIdx, sid) = partPrompt()
+
+  # Get Challenge
+  (login, ch, state, ch_aux) = getChallenge(login, sid) #sid is the "part identifier"
+  if((not login) or (not ch) or (not state)):
+    # Some error occured, error string in first return element.
+    print '\n!! Error: %s\n' % login
+    return
+
+  # Attempt Submission with Challenge
+  ch_resp = challengeResponse(login, password, ch)
+  (result, string) = submitSolution(login, ch_resp, sid, output(partIdx), \
+                                  source(partIdx), state, ch_aux)
+
+  print '== %s' % string.strip()
+
+
+# =========================== LOGIN HELPERS - NO NEED TO CONFIGURE THIS =======================================
+
+def loginPrompt():
+  """Prompt the user for login credentials. Returns a tuple (login, password)."""
+  (login, password) = basicPrompt()
+  return login, password
+
+
+def basicPrompt():
+  """Prompt the user for login credentials. Returns a tuple (login, password)."""
+  login = raw_input('Login (Email address): ')
+  password = raw_input('One-time Password (from the assignment page. This is NOT your own account\'s password): ')
+  return login, password
+
+def partPrompt():
+  print 'Hello! These are the assignment parts that you can submit:'
+  counter = 0
+  for part in partFriendlyNames:
+    counter += 1
+    print str(counter) + ') ' + partFriendlyNames[counter - 1]
+  partIdx = int(raw_input('Please enter which part you want to submit (1-' + str(counter) + '): ')) - 1
+  return (partIdx, partIds[partIdx])
+
+def getChallenge(email, sid):
+  """Gets the challenge salt from the server. Returns (email,ch,state,ch_aux)."""
+  url = challenge_url()
+  values = {'email_address' : email, 'assignment_part_sid' : sid, 'response_encoding' : 'delim'}
+  data = urllib.urlencode(values)
+  req = urllib2.Request(url, data)
+  response = urllib2.urlopen(req)
+  text = response.read().strip()
+
+  # text is of the form email|ch|signature
+  splits = text.split('|')
+  if(len(splits) != 9):
+    print 'Badly formatted challenge response: %s' % text
+    return None
+  return (splits[2], splits[4], splits[6], splits[8])
+
+def challengeResponse(email, passwd, challenge):
+  sha1 = hashlib.sha1()
+  sha1.update("".join([challenge, passwd])) # hash the first elements
+  digest = sha1.hexdigest()
+  strAnswer = ''
+  for i in range(0, len(digest)):
+    strAnswer = strAnswer + digest[i]
+  return strAnswer 
+  
+def challenge_url():
+  """Returns the challenge url."""
+  return "https://class.coursera.org/" + URL + "/assignment/challenge"
+
+def submit_url():
+  """Returns the submission url."""
+  return "https://class.coursera.org/" + URL + "/assignment/submit"
+
+def submitSolution(email_address, ch_resp, sid, output, source, state, ch_aux):
+  """Submits a solution to the server. Returns (result, string)."""
+  source_64_msg = email.message.Message()
+  source_64_msg.set_payload(source)
+  email.encoders.encode_base64(source_64_msg)
+
+  output_64_msg = email.message.Message()
+  output_64_msg.set_payload(output)
+  email.encoders.encode_base64(output_64_msg)
+  values = { 'assignment_part_sid' : sid, \
+             'email_address' : email_address, \
+             'submission' : output_64_msg.get_payload(), \
+             'submission_aux' : source_64_msg.get_payload(), \
+             'challenge_response' : ch_resp, \
+             'state' : state \
+           }
+  url = submit_url()  
+  data = urllib.urlencode(values)
+  req = urllib2.Request(url, data)
+  response = urllib2.urlopen(req)
+  string = response.read().strip()
+
+  result = 0
+  return result, string
+
+## This collects the source code (just for logging purposes) 
+def source(partIdx):
+  # open the file, get all lines
+  f = open(sourceFiles[partIdx])
+  src = f.read() 
+  f.close()
+  return src
+
+
+
+############ BEGIN ASSIGNMENT SPECIFIC CODE - YOU'LL HAVE TO EDIT THIS ##############
+
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.link import TCLink
+from CustomTopo import *
+
+# Make sure you change this string to the last segment of your class URL.
+# For example, if your URL is https://class.coursera.org/pgm-2012-001-staging, set it to "pgm-2012-001-staging".
+URL = 'sdn-001'
+
+# the "Identifier" you used when creating the part
+partIds = ['m3a']                        
+# used to generate readable run-time information for students
+partFriendlyNames = ['Create Custom Topology'] 
+# source files to collect (just for our records)
+sourceFiles = ['CustomTopo.py']                           
+          
+def output(partIdx):
+  """Uses the student code to compute the output for test cases."""
+  outputString = ''
+  
+  if partIdx == 0: # This is m3a
+    "Set up link parameters"
+    print "a. Setting link parameters"
+    "--- core to aggregation switches"
+    linkopts1 = {'bw':50, 'delay':'5ms'}
+    "--- aggregation to edge switches"
+    linkopts2 = {'bw':30, 'delay':'10ms'}
+    "--- edge switches to hosts"
+    linkopts3 = {'bw':10, 'delay':'15ms'}
+  
+    "Creating network and run simple performance test"
+    print "b. Creating Custom Topology"
+    topo = CustomTopo(linkopts1, linkopts2, linkopts3, fanout=3)
+
+    print "c. Firing up Mininet"
+    net = Mininet(topo=topo, link=TCLink)
+    net.start()
+    h1 = net.get('h1')
+    h27 = net.get('h27')
+  
+    print "d. Starting Test"
+    # Start pings
+    outputString = h1.cmd('ping', '-c6', h27.IP())
+    
+    print "e. Stopping Mininet"
+    net.stop()
+    
+  return outputString.strip()
+
+submit()
