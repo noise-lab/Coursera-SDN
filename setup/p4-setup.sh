77431df444432600f477ed43ad260a84064113ff
Nick Feamster <feamster@cs.princeton.edu>
2015-05-30 09:04:19 -0400

    initial setup script for p4 based on the P4 factory

---
+++ setup/p4-setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd ~
+git clone git@github.com:p4lang/p4factory.git
+
+cd ~/p4-factory/
+./install.sh
+
+sudo ~/p4factory/tools/veth_setup.sh
+
+cd ~/p4factory/targets/basic_routing/  
+make bm  
+sudo ./behavioral_model  
+
+cd ~/p4factory/targets/simple_router/  
+make bm
